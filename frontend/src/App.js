887c27a141d59c7f2cd1e0a3fd07968057d9fc89
Sulav Neupane <neupsulav@gmail.com>
2024-08-02 19:58:27 +0545

    Basic layout for home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Sidebar from "./components/sidebar/Sidebar";
-import Newsfeed from "./components/newsfeed/Newsfeed";
-import SuggestionBar from "./components/suggestionsbar/SuggestionBar";
-import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/Sidebar";
+import Newsfeed from "./components/Newsfeed";
+import SuggestionBar from "./components/SuggestionBar";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
