2cacf12702b63983bae6e18c0e48a06288fc624c
Sulav Neupane <neupsulav@gmail.com>
2024-08-04 16:19:52 +0545

    Sidebar added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import {
+  FaHome,
+  FaUser,
+  FaBriefcase,
+  FaUsers,
+  FaBell,
+  FaCog,
+} from "react-icons/fa";
 
 const Sidebar = () => {
   return (
@@ -7,6 +15,44 @@ const Sidebar = () => {
         <img src="https://storage2.timheuer.com/boratcircle.png" alt="logo" />
         <p>HamroSanjal</p>
       </div>
+      <ul className="sidebar-menu">
+        <li className="sidebar-item">
+          <a href="#home">
+            <FaHome className="sidebar-icon" />
+            Home
+          </a>
+        </li>
+        <li className="sidebar-item">
+          <a href="#profile">
+            <FaUser className="sidebar-icon" />
+            Profile
+          </a>
+        </li>
+        <li className="sidebar-item">
+          <a href="#jobs">
+            <FaBriefcase className="sidebar-icon" />
+            Jobs
+          </a>
+        </li>
+        <li className="sidebar-item">
+          <a href="#network">
+            <FaUsers className="sidebar-icon" />
+            Network
+          </a>
+        </li>
+        <li className="sidebar-item">
+          <a href="#notification">
+            <FaBell className="sidebar-icon" />
+            Notification
+          </a>
+        </li>
+        <li className="sidebar-item">
+          <a href="#settings">
+            <FaCog className="sidebar-icon" />
+            Settings
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
