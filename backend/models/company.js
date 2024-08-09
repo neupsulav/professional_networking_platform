54cf6f115e16989972b5ac25ab391465a75213a1
Sulav Neupane <neupsulav@gmail.com>
2024-08-09 09:55:20 +0545

    Added company schema

--- backend/models/company.js
+++ backend/models/company.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const companySchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -40,4 +40,4 @@ const userSchema = new mongoose.Schema({
 });
 
 //   exporting the user schema
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Company", companySchema);
