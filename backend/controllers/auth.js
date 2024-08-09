ee38dd8ab61f6368ae10a79f2a8138abf2380006
Sulav Neupane <neupsulav@gmail.com>
2024-08-09 10:01:06 +0545

    Added multer for image upload during registration

---
+++ backend/controllers/auth.js
@@ -0,0 +1,4 @@
+const User = require("../models/user");
+const Company = require("../models/company");
+const ErrorHandler = require("../middlewares/errorHandler");
+const catchAsync = require("../middlewares/catchAsync");
---
+++ backend/routes/auth.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+
+// // multer for image upload
+const FILE_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error("invalid image type");
+
+    if (isValid) {
+      uploadError = null;
+    }
+    cb(uploadError, "public/uploads/userImages");
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.split(" ").join("-");
+    const extension = FILE_TYPE_MAP[file.mimetype];
+    cb(null, `${Math.floor(Math.random() * 100000)}${Date.now()}.${extension}`);
+  },
+});
+
+const uploadOptions = multer({ storage: storage });
+
+module.exports = router;
