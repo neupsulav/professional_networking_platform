d71967bcf015861b95473213ee2826457d673ef4
Sulav Neupane <neupsulav@gmail.com>
2024-08-10 21:17:43 +0545

    Added People you may know section in UI

--- frontend/src/components/SuggestionBar.jsx
+++ frontend/src/components/SuggestionBar.jsx
@@ -3,7 +3,189 @@ import React from "react";
 const SuggestionBar = () => {
   return (
     <div className="suggestionbar_container">
-      <p>This is suggestions bar</p>
+      <p className="suggestionbar_heading">People you may know</p>
+      <div className="suggestionbar_items">
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+        <div className="suggestionbar_item">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+            alt="profile"
+          />
+          <div>
+            <p className="suggestionbar_item_name">Sulav Neupane</p>
+            <p className="suggestionbar_item_position">Full stack developer</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
