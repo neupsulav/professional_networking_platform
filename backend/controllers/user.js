cea747ac25dc11ed8e2519adaef96c047f0de5d3
Sulav Neupane <neupsulav@gmail.com>
2024-08-21 09:54:01 +0545

    Minor change in get self profile data API

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -60,7 +60,7 @@ const selfProfileData = catchAsync(async (req, res, next) => {
   //   user's profile data
   const userProfileData = await User.findOne({ _id: userId })
     .select(
-      "_id name username email image followers following following_company"
+      "_id name username email image followers following following_company createdAt"
     )
     .populate({ path: "followers", select: "name username image _id" })
     .populate({ path: "following", select: "name username image _id" })
