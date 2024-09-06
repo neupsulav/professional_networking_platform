6b2b813e20c54bd38dfd756fec41dff74a348757
Sulav Neupane <neupsulav@gmail.com>
2024-09-06 17:00:01 +0545

    UI ehnancement in applicants list modal

--- frontend/src/components/ApplicantLists.jsx
+++ frontend/src/components/ApplicantLists.jsx
@@ -4,26 +4,34 @@ const ApplicantsList = ({ applicants }) => {
   return (
     <div className="applicants-list-container">
       <h1 className="applicants-list-title">List of Applicants</h1>
-      {applicants.map((applicant, index) => (
-        <div key={index} className="applicant-card">
-          <img
-            src={applicant.user.image}
-            alt={applicant.name}
-            className="applicant-image"
-          />
-          <div className="applicant-info">
-            <h2 className="applicant-name">{applicant.name}</h2>
-            <p className="applicant-email">{applicant.email}</p>
-            <p className="applicant-phone">{applicant.phone}</p>
+      {applicants.length > 0 ? (
+        applicants.map((applicant, index) => (
+          <div key={index} className="applicant-card">
+            <img
+              src={applicant.user.image}
+              alt={applicant.name}
+              className="applicant-image"
+            />
+            <div className="applicant-info">
+              <h2 className="applicant-name">{applicant.name}</h2>
+              <p className="applicant-email">{applicant.email}</p>
+              <p className="applicant-phone">{applicant.phone}</p>
+            </div>
+            <button
+              className="view-cv-button"
+              onClick={() => window.open(applicant.user.cv, "_blank")}
+            >
+              View CV
+            </button>
           </div>
-          <button
-            className="view-cv-button"
-            onClick={() => window.open(applicant.user.cv, "_blank")}
-          >
-            View CV
-          </button>
-        </div>
-      ))}
+        ))
+      ) : (
+        <p
+          style={{ textAlign: "center", fontWeight: "bold", fontSize: "18px" }}
+        >
+          No one has applied to this job yet
+        </p>
+      )}
     </div>
   );
 };
