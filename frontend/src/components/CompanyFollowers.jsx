36f369d92d005fa9faf8527850998249bb552057
Sulav Neupane <neupsulav@gmail.com>
2024-09-07 11:12:13 +0545

    Integrated API to show followers list for company and added navigation to user profile

--- frontend/src/components/CompanyFollowers.jsx
+++ frontend/src/components/CompanyFollowers.jsx
@@ -1,190 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import Cookies from "universal-cookie";
+import { useNavigate } from "react-router-dom";
 
 const CompanyFollowers = () => {
+  const navigate = useNavigate();
+
+  // for cookies
+  const cookies = new Cookies();
+  const cookie = cookies.get("jwtToken");
+
+  // to store followers data
+  const [followers, setFollowers] = useState();
+  const [isDataFetched, setIsDataFetched] = useState(false);
+
+  // to get followers data
+  const getFollowers = async () => {
+    const res = await fetch("api/getcompanyselfprofile", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${cookie}`,
+      },
+    });
+
+    const response = await res.json();
+    setFollowers(response.company.followers);
+    setIsDataFetched(true);
+  };
+
+  useState(() => {
+    getFollowers();
+  }, []);
+
   return (
     <div className="suggestionbar_container">
       <p className="suggestionbar_heading">Followers</p>
       <div className="suggestionbar_items">
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
-        <div className="suggestionbar_item">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
-            alt="profile"
-          />
-          <div>
-            <p className="suggestionbar_item_name">Sulav Neupane</p>
-            <p className="suggestionbar_item_position">Full stack developer</p>
-          </div>
-        </div>
+        {isDataFetched &&
+          followers.length > 0 &&
+          followers.map((follower, index) => {
+            return (
+              <div key={index} className="suggestionbar_item_company">
+                <img src={follower.image} alt="profile" />
+                <div>
+                  <p
+                    className="suggestionbar_item_name"
+                    onClick={() => {
+                      navigate(`/user/${follower._id}`);
+                    }}
+                  >
+                    {follower.name}
+                  </p>
+                  <p className="suggestionbar_item_position">
+                    {follower.email}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
