adcc7642d68e9943c023b3d37391b8d8ff2625e1
Sulav Neupane <neupsulav@gmail.com>
2024-09-24 06:55:49 +0545

    Changed user notification UI

--- frontend/src/components/NotificationItem.jsx
+++ frontend/src/components/NotificationItem.jsx
@@ -3,27 +3,19 @@ import { useNavigate } from "react-router-dom";
 
 const NotificationItem = ({ details }) => {
   const navigate = useNavigate();
-  // const { type, user, post } = notification;
-
-  // const getMessage = () => {
-  //   switch (type) {
-  //     case "like":
-  //       return `${user} liked ${post}`;
-  //     case "comment":
-  //       return `${user} commented on ${post}`;
-  //     default:
-  //       return "";
-  //   }
-  // };
 
   return (
     <div
-      className="notification-item"
-      onClick={() => {
-        navigate(`/post/${details.post}`);
-      }}
+      className="notification-item-container"
+      onClick={() => navigate(`/post/${details.post}`)}
     >
-      <p>{details.content}</p>
+      <div className="notification-avatar">
+        <img src={details.user.image} alt={`${details.user}'s img`} />
+      </div>
+      <div className="notification-content">
+        <p className="notification-message">{details.content}</p>
+        <span className="notification-time">{details.time}</span>
+      </div>
     </div>
   );
 };
